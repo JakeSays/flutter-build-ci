f1947e2ef97b5111bed5f55ac38c955d6cf25e60
Jake Helfert <jake@helfert.us>
2026-03-27 18:48:55 -0600

    Updated readme, added cron schedule to watcher script (twice daily run), removed test app.

--- test_app/README.md
+++
@@ -1,3 +0,0 @@
-# test_app
-
-Just an empty test app that quits immediately with exit-code 0 after starting up.
--- test_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print('test app started up successfully. exiting...');
-  exit(0);
-}
